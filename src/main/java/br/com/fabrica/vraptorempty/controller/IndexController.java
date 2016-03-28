a561a066ee8ba3ba6599dc9b7da0c04f5ee3d833
Petros8 <fernandopetros@gmail.com>
2016-03-28 18:13:43 -0300

    first commit

---
+++ src/main/java/br/com/fabrica/vraptorempty/controller/IndexController.java
@@ -0,0 +1,18 @@
+package br.com.fabrica.vraptorempty.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+
+@Controller
+public class IndexController {
+
+	@Get("/index")
+	public void index(){
+		
+	}
+	
+	@Get("/index2")
+	public void index2(){
+		
+	}
+}
